864814011bd1b22a7b8115ed04db071c22cf2121
Punxious <hot1989hot@hotmail.com>
2021-03-07 00:10:17 -0500

    add verify process

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express')
+const verify = require('./verify')
 const app = express()
 app.use(express.json())
 const port = process.env.PORT || 8080;
+
+
+app.use(function (req, res, next) {
+    const verified = verify.Verify(req.headers['X-Signature-Ed25519'], req.headers['X-Signature-Timestamp']);
+    if (!verified) {
+        res.status(401).send('invalid request signature');
+    }
+    next()
+})
+
+app.get('/', (req, res) => {
+    res.send("Hello World!!")
+})
 
 app.post('/', (req, res) => {
     console.error(req.body);
---
+++ verify.js
@@ -0,0 +1,19 @@
+const nacl = require('tweetnacl');
+
+// Your public key can be found on your application in the Developer Portal
+const PUBLIC_KEY = process.env.PUBLIC_KEY || 'c833e91c733a5cc352a03b3cd29c61a2b1eac12e86cb966b5868c2ae615ced3c';
+
+function Verify(signature, timestamp) {
+    // const signature = req.get('X-Signature-Ed25519');
+    // const timestamp = req.get('X-Signature-Timestamp');
+    const body = req.rawBody;
+
+    const isVerified = nacl.sign.detached.verify(
+        Buffer.from(timestamp + body),
+        Buffer.from(signature, 'hex'),
+        Buffer.from(PUBLIC_KEY, 'hex')
+    );
+    return isVerified;
+}
+
+export function Verify();
