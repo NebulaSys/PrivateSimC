71978e48af0e338f6db05d7287b10d1c91bdb463
Punxious <hot1989hot@hotmail.com>
2021-03-06 20:00:46 -0500

    update ack ping response

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 const port = process.env.PORT || 8080;
 
 app.post('/', (req, res) => {
-    console.log(req.body)
-    res.send("Hello World!!")
+    const a = {};
+    if (req.body.hasOwnProperty('type') && req.body.type === 1) {
+        res.send(JSON.stringify({
+            'type': 1
+        }))
+    } else {
+        res.send("OPPS something went wrong..")
+    }
 })
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
 
