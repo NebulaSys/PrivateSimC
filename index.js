8dfe2af8d4032ffab66841446fac59a7d8b44e2b
Punxious <hot1989hot@hotmail.com>
2021-03-06 20:07:01 -0500

    test

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ app.use(express.json())
 const port = process.env.PORT || 8080;
 
 app.post('/', (req, res) => {
-    const a = {};
+    console.error(req.body);
     if (req.body.hasOwnProperty('type') && req.body.type === 1) {
         res.send(JSON.stringify({
             'type': 1
         }))
     } else {
-        console.error(req.body);
         res.send("OPPS something went wrong..")
     }
 })
