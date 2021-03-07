576ce72582382f69d4007781e13b5d4848b7d429
Punxious <hot1989hot@hotmail.com>
2021-03-06 20:13:48 -0500

    update response type to json object

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const port = process.env.PORT || 8080;
 app.post('/', (req, res) => {
     console.error(req.body);
     if (req.body.hasOwnProperty('type') && req.body.type === 1) {
-        res.send(JSON.stringify({
+        res.send({
             'type': 1
-        }))
+        })
     } else {
         res.send("OPPS something went wrong..")
     }
