203044a6319e2f32ce263d51352df972cad0e307
Punxious <hot1989hot@hotmail.com>
2021-03-12 00:41:17 -0500

    update

--- index.js
+++ index.js
@@ -1,61 +1,56 @@
 const express = require('express')
 const { spawn, exec } = require('child_process');
-var path = require('path');
+const { Storage } = require('@google-cloud/storage');
+const path = require('path');
 
 const verify = require('./verify')
 const app = express()
 const port = process.env.PORT || 8080;
-
-// exec('bash -c ./simc', (error, stdout, stderr) => {
-//     if (error) {
-//         console.error(`error: ${error.message}`);
-//         return;
-//     }
-
-//     if (stderr) {
-//         console.error(`stderr: ${stderr}`);
-//         return;
-//     }
-
-//     console.log(`stdout:\n${stdout}`);
-
-// })
-
 app.use(express.json())
 
-// app.use(function (req, res, next) {
-//     console.log(JSON.stringify(req.headers));
-//     console.log(JSON.stringify(req.body));
-
-//     const verified = verify.Verify(req.get('X-Signature-Ed25519'), req.get('X-Signature-Timestamp'), JSON.stringify(req.body));
-//     if (!verified) {
-//         res.status(401).send('invalid request signature');
-//         return;
-//     }
-//     next();
-// });
-
-app.get('/simc/:server/:realm/:char', (req, res) => {
+app.post('/simc/:server/:realm/:char', (req, res) => {
     if (req.params.hasOwnProperty('server') && req.params.hasOwnProperty('realm') && req.params.hasOwnProperty('char')) {
+        console.log("req.body: ", req.body)
+
         const server = req.params.server;
         const realm = req.params.realm;
         const char = req.params.char;
+        const bucketName = 'simc.intertrick.com';
+        const filename = `./${char}.html`;
+        const timestamp = new Date().toISOString();
+        destination = `${server}/${realm}/${char}.${timestamp}.html`
+
         console.log("server", server);
         console.log("realm", realm);
         console.log("char", char);
-        const sim = exec(`./simc armory=${server},${realm},${char} calculate_scale_factors=1 html=${char}.html`);
-        // const sim = exec(`./simc armory=${server},${realm},${char} html=${char}.html`);
-        // const sim = exec(`./simc armory=us,illidan,punxious html=a.html`);
-        
-        
+        res.send(`http://${bucketName}/${destination}`);
+        // const sim = exec(`./simc armory=${server},${realm},${char} calculate_scale_factors=1 html=${char}.html`);
+        const sim = exec(`./simc armory=${server},${realm},${char} html=${char}.html`);
+
         sim.stdout.on('data', (data) => {
             console.log(`stdout: ${data}`);
         });
         sim.stderr.on('data', (data) => {
             console.log(`ERROR!!! ${data}`)
-          });
-        sim.on('close', (code) => {
-            res.sendFile(path.join(__dirname + `/${char}.html`));
+        });
+        sim.on('close', async (code) => {
+            const storage = new Storage();
+
+            // const sim = exec(`./simc armory=${server},${realm},${char} html=${char}.html`);
+            // const sim = exec(`./simc armory=us,illidan,punxious html=a.html`);
+            await storage.bucket(bucketName).upload(filename, {
+                // By setting the option `destination`, you can change the name of the
+                // object you are uploading to a bucket.
+                destination: destination,
+                metadata: {
+                    // Enable long-lived HTTP caching headers
+                    // Use only if the contents of the file will never change
+                    // (If the contents will change, use cacheControl: 'no-cache')
+                    cacheControl: 'public, max-age=31536000',
+                },
+            });
+            console.log(`${filename} uploaded to ${bucketName}.`);
+            // res.redirect(`http://simc.intertrick.com/${destination}`);
         });
     } else {
         res.send("Something went wrong...")
